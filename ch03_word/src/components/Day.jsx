3d74f2c68d795efc3991922fc2971dc37670c01a
kahng <gloomy5480@hotmail.com>
2025-05-13 18:08:40 +0900

    5월 13일 수업 끝

--- ch03_word/src/components/Day.jsx
+++ ch03_word/src/components/Day.jsx
@@ -3,12 +3,14 @@ import { useParams } from "react-router-dom";
 import dummy from "../db/data.json";
 import Word from "./word";
 import useFetch from "../hooks/useFetch";
+import { supabase } from "../lib/supabaseClient";
 
 export default function Day() {
   //<Route path="/day/:day" element={<Day></Day>} />
   const { day } = useParams();
 
-  const words = useFetch(`http://localhost:3010/words?day=${day}`);
+  const { data, error } = fetchWords(day);
+  // const words = useFetch(`http://localhost:3010/words?day=${day}`);
   // const [words, setWords] = useState([]); //hooks를 사용하지 않고 fetch를 사용한 경우.
   // useEffect(() => {
   //   fetch(`http://localhost:3010/words?day=${day}`)
